a24c4e180b9c46c8b9402a6187bd63fbc3b00381
Richard Wackerbarth <rkw@dataplex.net>
2017-11-26 07:30:18 -0600

    Bed Autotune Menu Feature Configuration

---
+++ Marlin/src/config/custom/PR_8365/FeatureConfiguration.h
@@ -0,0 +1,6 @@
+// These values will need to be added to the configuration files
+
+#define BEDPID_EDIT           // Add PID values editing menu
+#define BEDPID_AUTOTUNE_MENU  // Add PID Autotune to the LCD "Temperature" menu to run M303 and apply the result.
+#define BED_MIN_HEATED_OPERATING_TEMP 50
+#define BED_MAX_HEATED_OPERATING_TEMP BED_MAXTEMP - 5
